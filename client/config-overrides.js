2d15a7d77dd045e2c597275831766c93b53409e4
sebuszqo <sekn.kn@gmail.com>
2023-03-25 16:16:47 +0100

    Added types connection between front-end and back-end

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,5 +1,14 @@
 const {override} = require('customize-cra');
 const {aliasDangerous, configPaths} = require('react-app-rewire-alias/lib/aliasDangerous');
+const { removeModuleScopePlugin } = require('customize-cra');
 
+module.exports = {webpack: override(aliasDangerous(configPaths('./tsconfig.paths.json'))),}
 
-module.exports = {webpack: override(aliasDangerous(configPaths('./tsconfig.paths.json')))};
+module.exports = function override(config, env) {
+    if (!config.plugins) {
+        config.plugins = [];
+    }
+    removeModuleScopePlugin()(config);
+
+    return config;
+};
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '../public/vite.svg'
 import './App.css'
-import {TestInterface} from "types";
 
 function App() {
   const [count, setCount] = useState(0)
