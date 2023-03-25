f8d7bae4d341e276fb2772dd7ec98f25f2ab95a7
sebuszqo <sekn.kn@gmail.com>
2023-03-25 15:57:33 +0100

    Added types connection between front-end and back-end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {TestInterface} from "types";
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
