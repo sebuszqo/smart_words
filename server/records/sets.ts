e215dd4df25c08d01e5a67e0fba5c1f4358d3566
sebuszqo <sekn.kn@gmail.com>
2023-04-01 21:47:05 +0200

    Added new class SetRecord to represent and to deal with database requests, what's more added 2 new methods to the class - findOne(:id) - to find one object in database and findAll() - to find all objects in database, both are mapped to be instances of a SetRecord class

--- server/records/sets.ts
+++ server/records/sets.ts
@@ -19,3 +19,69 @@
 //   }
 // }
 // run();
+
+import { ISetWithWords, IWords } from "../types";
+import { ValidationError } from "../errors/error";
+import { WordSet } from "../database/models/wordsets";
+import { Document } from "mongoose";
+interface Word {
+  word: string;
+  meaning: string;
+  _id: string;
+}
+
+interface Set extends Document {
+  __v: number;
+  _id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  words: Word[];
+}
+
+export class SetRecord implements ISetWithWords {
+  public _id?: string;
+  public description: string;
+  public name: string;
+  public createdAt: string;
+  public words: IWords[];
+  public __v: number;
+
+  constructor(obj: Set) {
+    if (!obj.name || obj.name.length > 100) {
+      throw new ValidationError("Name cannot be empty or longer than 100.");
+    }
+
+    if (obj.description) {
+      if (obj.description.length > 1000) {
+        throw new ValidationError("Description cannot be longer than 1000.");
+      }
+    }
+    if (!obj.words) {
+      throw new ValidationError("your ser of words cannot be empty!");
+    }
+    this._id = obj._id;
+    this.name = obj.name;
+    this.description = !obj.description ? null : obj.description;
+    this.createdAt = obj.createdAt;
+    this.words = obj.words;
+    this.__v = obj.__v;
+  }
+
+  static async findOne(id: string): Promise<SetRecord | null> {
+    try {
+      const result = await WordSet.findById(`${id}`);
+      return new SetRecord(result.toObject());
+    } catch (e) {
+      return null;
+    }
+  }
+  static async findAll(): Promise<SetRecord[] | null> {
+    try {
+      const result = await WordSet.find();
+      return result.map((set) => new SetRecord(set.toObject()));
+    } catch (e) {
+      return null;
+    }
+  }
+}
