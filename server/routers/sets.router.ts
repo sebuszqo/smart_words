063ea516d80ac479c56db8421edad28670b7faf8
sebuszqo <sekn.kn@gmail.com>
2023-04-01 22:15:01 +0200

    Added new router path .get('/') and preparation for test cases.

--- server/routers/sets.router.ts
+++ server/routers/sets.router.ts
@@ -4,19 +4,21 @@ import { SetRecord } from "../records/sets";
 export const setRouter = Router();
 
 setRouter
-  .get("/", (req: Request, res: Response) => {
-    //@TODO
-    //    This path is to get all of the available sets of words
-  })
-  .get("/search/:name?", (req: Request, res: Response) => {
-    //@TODO
-    //   This path is to get searched sets by name - if exists
-  })
   .get("/:id", async (req: Request, res: Response) => {
     const id = req.params;
     const set = await SetRecord.findOne(`${id}`);
     res.json(set);
   })
+  .get("/search/:name?", (req: Request, res: Response) => {
+    //@TODO
+    //   This path is to get searched sets by name - if exists
+  })
+
+  .get("/", async (req: Request, res: Response) => {
+    const sets = await SetRecord.findAll();
+    res.json(sets);
+  })
+
   .post("/", (req: Request, res: Response) => {
     // @TODO
     //    This path is to add new objects - new sets of words to database
