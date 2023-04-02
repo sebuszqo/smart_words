a8db396935ce982e6c969f8e2249326a8304752c
sebuszqo <sekn.kn@gmail.com>
2023-04-02 14:02:45 +0200

    Added TDD tests to check new methods such as .insert(), .findByName() and checking if throwing errors works properly.

--- server/tests/sets.records.methods.test.ts
+++ server/tests/sets.records.methods.test.ts
@@ -2,21 +2,105 @@ import { SetRecord } from "../records/sets";
 
 import mongoose from "mongoose";
 import { connectDB } from "../database/connections/mainDatabase";
+import { ValidationError } from "../errors/error";
+import { WordSet } from "../database/models/wordsets";
+
 let connection: mongoose.Connection;
 
 beforeAll(async () => {
   connection = await connectDB();
+  await WordSet.deleteMany({ name: "Test Set" });
 });
 
 afterAll(async () => {
   await connection.close();
 });
 
-// test("Finding a set by ID returns the correct set", async () => {
-//   const set = await SetRecord.findOne(defaultSet._id);
-//   expect(set).toBeInstanceOf(SetRecord);
-//   expect(set._id).toBe(defaultSet._id);
-// });
+afterEach(async () => {
+  await WordSet.deleteMany({ name: "Test Set" });
+});
+
+const defaultSet = {
+  name: "Test Set",
+  description: "A test set",
+  createdAt: new Date(),
+  words: [
+    { word: "test", meaning: "A trial or experiment" },
+    {
+      word: "example",
+      meaning:
+        "A thing characteristic of its kind or illustrating a general rule.",
+    },
+  ],
+};
+
+test("Inserting a new set creates a SetRecord instance", async () => {
+  const set = new SetRecord(defaultSet);
+  await set.insert();
+  expect(set).toBeInstanceOf(SetRecord);
+});
+
+test("Inserting a new set saves it to the database", async () => {
+  const set = new SetRecord({
+    name: "Test Set",
+    description: "A test set",
+    createdAt: new Date(),
+    words: [
+      { word: "apple", meaning: "fruit" },
+      { word: "banana", meaning: "fruit" },
+    ],
+  });
+  const insertedSet = await set.insert();
+  const foundSet = await SetRecord.findOne(insertedSet._id);
+  expect(foundSet).not.toBeNull();
+  expect(foundSet.name).toBe("Test Set");
+  expect(foundSet.description).toBe("A test set");
+  expect(foundSet.words.length).toBe(2);
+  expect(foundSet.words[0].word).toBe("apple");
+  expect(foundSet.words[0].meaning).toBe("fruit");
+});
+
+test("Inserting a new set saves it to the database", async () => {
+  const set = new SetRecord({
+    name: "Test Set",
+    description: "A test set",
+    createdAt: new Date(),
+    words: [
+      { word: "apple", meaning: "fruit" },
+      { word: "banana", meaning: "fruit" },
+    ],
+  });
+  const insertedSet = await set.insert();
+  await expect(async () => await insertedSet.insert()).rejects.toThrow(
+    ValidationError
+  );
+});
+
+test("Inserting a new set with the same name as an existing set throws a ValidationError", async () => {
+  await WordSet.deleteMany({ name: "Existing Set" });
+  const existingSet = new SetRecord({
+    name: "Existing Set",
+    description: "A test set",
+    createdAt: new Date(),
+    words: [
+      { word: "apple", meaning: "fruit" },
+      { word: "banana", meaning: "fruit" },
+    ],
+  });
+  await existingSet.insert();
+  const newSet = new SetRecord({
+    name: "Existing Set",
+    description: "A test set",
+    createdAt: new Date(),
+    words: [
+      { word: "apple", meaning: "fruit" },
+      { word: "banana", meaning: "fruit" },
+    ],
+  });
+  await expect(async () => await newSet.insert()).rejects.toThrowError(
+    ValidationError
+  );
+});
 
 test("Finding all sets returns an array of SetRecord instances", async () => {
   const sets = await SetRecord.findAll();
@@ -31,10 +115,12 @@ test("Finding all sets by searching '' - empty string - returns an array of SetR
 });
 
 test("Finding all sets with a name filter returns the correct sets", async () => {
+  const set = new SetRecord(defaultSet);
+  await set.insert();
   const sets = await SetRecord.findAll("Test");
   expect(sets).toBeInstanceOf(Array);
   expect(sets[0]).toBeInstanceOf(SetRecord);
-  // expect(sets[0].name).toBe("Test Set");
+  expect(sets[0].name).toBe("Test Set");
 });
 
 test("Finding all sets with a non-existent name filter returns an empty array", async () => {
