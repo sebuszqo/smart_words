9d290fb81a64e3a1461dc77d823a09f0e2ef656b
sebuszqo <sekn.kn@gmail.com>
2023-04-02 12:47:10 +0200

    Divided tests to 2 files, one to check creating instances of setRecord class - throws errors etc. Second to check methods works as they should

--- server/tests/sets.record.test.ts
+++ server/tests/sets.record.test.ts
@@ -94,28 +94,3 @@ test("Creating SetRecord with valid data creates the record", () => {
   });
   expect(set).toBeInstanceOf(SetRecord);
 });
-
-// test("Finding a set by ID returns the correct set", async () => {
-//   const set = await SetRecord.findOne(defaultSet._id);
-//   expect(set).toBeInstanceOf(SetRecord);
-//   expect(set._id).toBe(defaultSet._id);
-// });
-
-test("Finding all sets returns an array of SetRecord instances", async () => {
-  const sets = await SetRecord.findAll();
-  expect(sets).toBeInstanceOf(Array);
-  expect(sets[0]).toBeInstanceOf(SetRecord);
-});
-
-test("Finding all sets with a name filter returns the correct sets", async () => {
-  const sets = await SetRecord.findAll("Test");
-  expect(sets).toBeInstanceOf(Array);
-  expect(sets[0]).toBeInstanceOf(SetRecord);
-  // expect(sets[0].name).toBe("Test Set");
-});
-
-test("Finding all sets with a non-existent name filter returns an empty array", async () => {
-  const sets = await SetRecord.findAll("Non-existent name");
-  expect(sets).toBeInstanceOf(Array);
-  expect(sets).toHaveLength(0);
-});
