659668873785c15331e432de378042a3e6a19d81
sebuszqo <sekn.kn@gmail.com>
2023-04-05 23:00:47 +0200

    Added deleting sets by clicking a button.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./App.css";
 
 interface Set {
   name: string;
   description: string;
+  _id: string;
 }
 
 export function App() {
@@ -12,41 +13,47 @@ export function App() {
   const [searchBar, setSearchBar] = useState("");
   const [sets, setSets] = useState<Set[]>([]);
 
-  // useEffect(() => {
-  //   const textToWrite = "Search for your set!";
-  //   let index = 0;
-  //   let textLength = textToWrite.length;
-  //   const intervalId = setInterval(() => {
-  //     if (index === textLength - 1) {
-  //       clearInterval(intervalId);
-  //     } else {
-  //       setPlaceholderText((prevText) => prevText + textToWrite[index]);
-  //       index++;
-  //     }
-  //   }, 100);
-  //   return () => clearInterval(intervalId);
-  // }, []);
+  useEffect(() => {
+    const textToWrite = "SSearch for your set!";
+    let index = 0;
+    let textLength = textToWrite.length;
+    const intervalId = setInterval(() => {
+      if (index === textLength - 1) {
+        clearInterval(intervalId);
+      } else {
+        setPlaceholderText((prevText) => prevText + textToWrite[index]);
+        index++;
+      }
+    }, 100);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  function handleDeleteButtonClick(event: any) {
+    (async () => {
+      const response = await fetch(
+        `http://localhost:3001/set/${event.target.value}`,
+        { method: "DELETE" }
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      } else {
+        const updatedSets = sets.filter(
+          (set) => set._id !== event.target.value
+        );
+        setSets(updatedSets);
+      }
+    })();
+  }
 
   useEffect(() => {
     (async () => {
-      // console.log(searchBar);
       const response = await fetch(
         `http://localhost:3001/set/search/${searchBar}`
       );
       const sets = await response.json();
-      console.log(sets);
       setSets(sets);
     })();
   }, [searchBar]);
-
-  // const handleSearch = async () => {
-  //   const response = await fetch(
-  //       `http://localhost:3001/set/search?name=${searchBar}`
-  //   );
-  //   const data = await response.json();
-  //   setSets(data);
-  //   console.log(data);
-  // };
 
   return (
     <div className="App">
@@ -60,20 +67,21 @@ export function App() {
             placeholder={placeholderText}
             onChange={(e) => setSearchBar(e.target.value)}
           />
-          {/*<input type="submit" value="SEARCH" />*/}
         </div>
         <button className="add-new-set">ADD new set</button>
       </header>
       <main>
         <div className="card-container">
           {sets.map((obj) => (
-            <div className="card">
+            <div className="card" key={obj._id}>
               <div className="card-header">
                 <h2>{obj.name}</h2>
                 <button
-                  value={obj.name}
+                  value={obj._id}
                   className="delete-card-button"
                   aria-label="Delete Card"
+                  type={"button"}
+                  onClick={handleDeleteButtonClick}
                 >
                   🗑️
                 </button>
