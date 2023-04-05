5214888038a2759b598d9c0ec10f705cb77fc7f7
sebuszqo <sekn.kn@gmail.com>
2023-04-05 22:11:56 +0200

    Little change of App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,65 +1,87 @@
 import { useEffect, useState } from "react";
+
 import "./App.css";
+
+interface Set {
+  name: string;
+  description: string;
+}
 
 export function App() {
   const [placeholderText, setPlaceholderText] = useState("");
+  const [searchBar, setSearchBar] = useState("");
+  const [sets, setSets] = useState<Set[]>([]);
+
+  // useEffect(() => {
+  //   const textToWrite = "Search for your set!";
+  //   let index = 0;
+  //   let textLength = textToWrite.length;
+  //   const intervalId = setInterval(() => {
+  //     if (index === textLength - 1) {
+  //       clearInterval(intervalId);
+  //     } else {
+  //       setPlaceholderText((prevText) => prevText + textToWrite[index]);
+  //       index++;
+  //     }
+  //   }, 100);
+  //   return () => clearInterval(intervalId);
+  // }, []);
 
   useEffect(() => {
-    const textToWrite = "Search for your set!";
-    let index = 0;
-    let textLength = textToWrite.length;
-    const intervalId = setInterval(() => {
-      if (index === textLength - 1) {
-        clearInterval(intervalId);
-      } else {
-        setPlaceholderText((prevText) => prevText + textToWrite[index]);
-        index++;
-      }
-    }, 100);
+    (async () => {
+      const response = await fetch(
+        `http://localhost:3001/set/search/?name=${searchBar}`
+      );
+      const sets = await response.json();
+      console.log(sets);
+      setSets(sets);
+    })();
+  }, [searchBar]);
 
-    return () => clearInterval(intervalId);
-  }, []);
+  // const handleSearch = async () => {
+  //   const response = await fetch(
+  //       `http://localhost:3001/set/search?name=${searchBar}`
+  //   );
+  //   const data = await response.json();
+  //   setSets(data);
+  //   console.log(data);
+  // };
 
   return (
     <div className="App">
       <header>
         <h1>SmartWords</h1>
         <div id="search-bar">
-          <input type="text" id="text-to-write" placeholder={placeholderText} />
-          <input type="submit" value="SEARCH" />
+          <input
+            type="text"
+            value={searchBar}
+            id="text-to-write"
+            placeholder={placeholderText}
+            onChange={(e) => setSearchBar(e.target.value)}
+          />
+          {/*<input type="submit" value="SEARCH" />*/}
         </div>
         <button className="add-new-set">ADD new set</button>
       </header>
       <main>
         <div className="card-container">
-          <div className="card">
-            <h2>Card 1</h2>
-            <p>Description of Card 1 lorem</p>
-          </div>
-          <div className="card">
-            <h2>Card 2</h2>
-            <p>Description of Card 2</p>
-          </div>
-          <div className="card">
-            <h2>Card 3</h2>
-            <p>Description of Card 3</p>
-          </div>
-          <div className="card">
-            <h2>Card 4</h2>
-            <p>Description of Card 4</p>
-          </div>
-          <div className="card">
-            <h2>Card 5</h2>
-            <p>Description of Card 5</p>
-          </div>
-          <div className="card">
-            <h2>Card 6</h2>
-            <p>Description of Card 6</p>
-          </div>
-          <div className="card">
-            <h2>Card 7</h2>
-            <p>Description of Card 6</p>
-          </div>
+          {sets.map((obj) => (
+            <div className="card">
+              <div className="card-header">
+                <h2>{obj.name}</h2>
+                <button
+                  value={obj.name}
+                  className="delete-card-button"
+                  aria-label="Delete Card"
+                >
+                  🗑️
+                </button>
+              </div>
+              <div className="card-body">
+                <p>{obj.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </main>
     </div>
